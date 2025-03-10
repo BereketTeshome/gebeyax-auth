207d40ac6576819aa80ce3461cc5c8613664e4a7
Bereket <bereketteshome685@gmail.com>
2025-03-10 14:08:19 +0300

    registration updated

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(
   "postgres://gebeyax:PgGxRSA1%21%40%23@localhost:8001/gebeyax",
+  // "postgres://postgres:462462b@@localhost:5432/postgres",
   {
     dialect: "postgres",
     logging: false,
